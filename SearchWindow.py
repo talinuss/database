e70cab8e5e30aaea83f276675233f40df7564923
Talinuss <egor.belyi2012@gmail.com>
2021-02-14 22:02:33 +0700

    Первый коммит

---
+++ SearchWindow.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'SearchWindowKHKDpp.ui'
+##
+## Created by: Qt User Interface Compiler version 5.15.2
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+
+
+class Ui_MainWindow(object):
+    def setup(self, MainWindow):
+        if not MainWindow.objectName():
+            MainWindow.setObjectName(u"MainWindow")
+        MainWindow.resize(222, 246)
+        self.centralwidget = QWidget(MainWindow)
+        self.centralwidget.setObjectName(u"centralwidget")
+        self.Surname = QLabel(self.centralwidget)
+        self.Surname.setObjectName(u"Surname")
+        self.Surname.setGeometry(QRect(10, 40, 61, 20))
+        self.Secondname_search = QLineEdit(self.centralwidget)
+        self.Secondname_search.setObjectName(u"Secondname_data")
+        self.Secondname_search.setGeometry(QRect(80, 100, 131, 20))
+        self.ID = QLabel(self.centralwidget)
+        self.ID.setObjectName(u"ID")
+        self.ID.setGeometry(QRect(10, 10, 61, 21))
+        self.Phone_num = QLabel(self.centralwidget)
+        self.Phone_num.setObjectName(u"Phone_num")
+        self.Phone_num.setGeometry(QRect(10, 130, 61, 20))
+        self.Surname_search = QLineEdit(self.centralwidget)
+        self.Surname_search.setObjectName(u"Surname_data")
+        self.Surname_search.setGeometry(QRect(80, 40, 131, 20))
+        self.Secondname = QLabel(self.centralwidget)
+        self.Secondname.setObjectName(u"Secondname")
+        self.Secondname.setGeometry(QRect(10, 100, 61, 21))
+        self.Name = QLabel(self.centralwidget)
+        self.Name.setObjectName(u"Name")
+        self.Name.setGeometry(QRect(10, 70, 61, 21))
+        self.Name_search = QLineEdit(self.centralwidget)
+        self.Name_search.setObjectName(u"Name_data")
+        self.Name_search.setGeometry(QRect(80, 70, 131, 20))
+        self.Phone_num_search = QLineEdit(self.centralwidget)
+        self.Phone_num_search.setObjectName(u"Phone_num_data")
+        self.Phone_num_search.setGeometry(QRect(80, 130, 131, 20))
+        self.ID_search = QLineEdit(self.centralwidget)
+        self.ID_search.setObjectName(u"ID_data")
+        self.ID_search.setGeometry(QRect(80, 10, 131, 20))
+        self.button_search = QPushButton(self.centralwidget)
+        self.button_search.setObjectName(u"button_search")
+        self.button_search.setGeometry(QRect(10, 160, 201, 31))
+        font = QFont()
+        font.setFamily(u"Microsoft JhengHei UI")
+        font.setPointSize(14)
+        font.setBold(True)
+        font.setWeight(75)
+        self.button_search.setFont(font)
+        MainWindow.setCentralWidget(self.centralwidget)
+
+        self.retranslateUi(MainWindow)
+
+        QMetaObject.connectSlotsByName(MainWindow)
+    # setupUi
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
+        self.Surname.setText(QCoreApplication.translate("MainWindow", u"\u0424\u0430\u043c\u0438\u043b\u044f:", None))
+        self.ID.setText(QCoreApplication.translate("MainWindow", u"ID:", None))
+        self.Phone_num.setText(QCoreApplication.translate("MainWindow", u"\u0422\u0435\u043b\u0435\u0444\u043e\u043d:", None))
+        self.Secondname.setText(QCoreApplication.translate("MainWindow", u"\u041e\u0442\u0447\u0435\u0441\u0442\u0432\u043e:", None))
+        self.Name.setText(QCoreApplication.translate("MainWindow", u"\u0418\u043c\u044f:", None))
+        self.button_search.setText(QCoreApplication.translate("MainWindow", u"SEARCH", None))
+    # retranslateUi
+
---
+++ creation_of_DB.py
@@ -0,0 +1,23 @@
+import random as rd
+import sqlite3 as sql
+import os
+
+file_path = 'K:/_My_Docs/Database/data_base'
+if os.path.isfile(file_path):
+    os.remove(file_path)
+
+names = ['Егор', 'Данил', 'Андрей', 'Антон', 'Алексей']
+surnames = ['Белявцев', 'Прошалыкин', 'Буткеев', 'Басюра', 'Агеев']
+second_names = ['Сергеевич', 'Андреевичч', 'Витальевич', 'Тарасович', 'Павлович']
+phone_nums = ['+79236357970', '+79234906581', '+79045743116', '+79236315984', '+79913720869']
+
+users = [[surnames[i], names[i], second_names[i], phone_nums[i]] for i in range(len(names))]
+print(users)
+
+connection = sql.connect('data_base')
+cursor = connection.cursor()
+
+cursor.execute('CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, surname VARCHAR, name VARCHAR, second_name VARCHAR, phone_num VARCHAR)')
+cursor.executemany('INSERT INTO users (surname, name, second_name, phone_num) VALUES (?, ?, ?, ?)', users)
+
+connection.commit()
