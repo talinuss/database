233513cc0880a70be30fe028bcaedf6c9cb4cc65
Talinuss <egor.belyi2012@gmail.com>
2021-02-15 21:42:07 +0700

    Вновь добавлена функция сортировки

--- main.py
+++ main.py
@@ -8,21 +8,32 @@ class MainWindow(QtWidgets.QMainWindow, Main_Window.Ui_MainWindow):
         super().__init__()
         self.setupUi(self)
         self.link = 0
-        self.mass = self.read_db()
+        self.read_db()
         self.show_items(self.link)
+        self.button_sort.clicked.connect(self.sorted)
 
     def insert_db(self):
-        ID = str(self.ID_data.text())
         surname = str(self.Surame_data.text())
         name = str(self.Name_data.text())
         secondname = str(self.Secondname_data.text())
         phone_num = str(self.Phone_num_data.text())
+        new_row = [surname, name, secondname, phone_num]
+        connection = sql.connect('data_base')
+        cursor = connection.cursor()
+        cursor.execute('INSERT INTO users (surname, name, second_name, phone_num) VALUES (?, ?, ?, ?)', new_row)
+        cursor.close()
+        connection.commit()
+        connection.close()
+        self.link = 0
+        self.show_items(self.link)
 
     def read_db(self):
         connection = sql.connect('data_base')
         cursor = connection.cursor()
         self.mass = cursor.execute('SELECT * FROM users').fetchall()
-        return self.mass
+        cursor.close()
+        connection.close()
+        print(self.mass)
 
     def show_items(self, index):
         self.ID_data.setText(str(self.mass[index][0]))
@@ -31,8 +42,19 @@ class MainWindow(QtWidgets.QMainWindow, Main_Window.Ui_MainWindow):
         self.Secondname_data.setText(str(self.mass[index][3]))
         self.Phone_num_data.setText(str(self.mass[index][4]))
 
+    def sort(self):
+        if self.comboBox.currentIndex() == 0:
+            self.mass = sorted(self.mass, key = lambda k: k[0]) #ID
+        elif self.comboBox.currentIndex() == 1:
+            self.mass = sorted(self.mass, key = lambda k: k[1]) #Surname
+        elif self.comboBox.currentIndex() == 2:
+            self.mass = sorted(self.mass, key = lambda k: k[2]) #Name
+        elif self.comboBox.currentIndex() == 3:
+            self.mass = sorted(self.mass, key = lambda k: k[3]) #Secondname
 
-
+    def sorted(self):
+        self.sort()
+        self.show_items(self.link)
 
 
 def main():
