fe266b4e31f05f57ddff202686460fd844ba73cc
talinuss <77614819+talinuss@users.noreply.github.com>
2021-02-28 12:47:32 +0700

    Add files via upload
    добавлено отображение в таблице пользователей из поиска

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 import sys
 import sqlite3 as sql
 from PyQt5 import QtWidgets
-import MainWindow
-import SearchWindow
+import Main_Window
+import Search_Window
 
-class SearchWindow(QtWidgets.QMainWindow, SearchWindow.Ui_MainWindow):
+class SearchWindow(QtWidgets.QMainWindow, Search_Window.Ui_MainWindow):
     def __init__(self, root):
         super().__init__(root)
         self.setup(self)
         self.main = root
         self.button_search.clicked.connect(self.search)
-        
+
     def search(self):
         connection = sql.connect('data_base')
         cursor = connection.cursor()
@@ -32,7 +32,6 @@ class SearchWindow(QtWidgets.QMainWindow, SearchWindow.Ui_MainWindow):
             else:
                 request = request + ' surname = "'+ surname + '"'
 
-
         if name != '':
             if request != '':
                 request =  request + ' AND name = "'+ name + '"'
@@ -44,27 +43,47 @@ class SearchWindow(QtWidgets.QMainWindow, SearchWindow.Ui_MainWindow):
                 request =  request + ' AND second_name = "'+ second_name + '"'
             else:
                 request =  request + ' second_name = "'+ second_name + '"'
-                
-
+              
         if phone_num != '':
             if request != '':
                 request = request + ' AND phone_num = "'+ phone_num + '"'
             else:
                 request = request + ' phone_num = "'+ phone_num + '"'
         
-        req = 'SELECT * FROM users where ('+ request + ')'
-        print(req)
-        self.mass_search = cursor.execute(req).fetchall()
-        print(self.mass_search)
+        req = 'SELECT * FROM users where ('+ request +')'
+        try:
+            self.mass_search = cursor.execute(req).fetchall()
+            print(req)
+            print(self.mass_search)
+        except:
+            self.mass_search = False
+            print('Oops... There is no request')
+
 
         cursor.close()
         connection.close()
+
+        self.search_show()
         
     def search_show(self):
-        pass
+        connection = sql.connect('data_base')
+        cursor = connection.execute('SELECT * FROM users')
+
+        if self.mass_search:
+            names = list(map(lambda x: x[0], cursor.description))
+            self.table.setColumnCount(len(self.mass_search[0]))
+            self.table.setRowCount(len(self.mass_search))
+            self.table.setHorizontalHeaderLabels(names)
+
+            for i in range(len(self.mass_search)):
+                for j in range(len(self.mass_search[i])):
+                    self.table.setItem( i, j, QtWidgets.QTableWidgetItem(str(self.mass_search[i][j])) )
+
+        cursor.close()
+        connection.close()
 
 
-class MainWindow(QtWidgets.QMainWindow, MainWindow.Ui_MainWindow):
+class MainWindow(QtWidgets.QMainWindow, Main_Window.Ui_MainWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
@@ -75,12 +94,13 @@ class MainWindow(QtWidgets.QMainWindow, MainWindow.Ui_MainWindow):
         self.button_next.clicked.connect(self.next)
         self.button_last.clicked.connect(self.last)
         self.button_search.clicked.connect(self.search)
+        self.button_insert.clicked.connect(self.insert_db)
 
     def insert_db(self):
-        surname = str(self.Surame_data.text())
-        name = str(self.Name_data.text())
-        second_name = str(self.Second_name_data.text())
-        phone_num = str(self.Phone_num_data.text())
+        surname = str(self.Surname_data.text()).strip()
+        name = str(self.Name_data.text()).strip()
+        second_name = str(self.Second_name_data.text()).strip()
+        phone_num = str(self.Phone_num_data.text()).strip()
         new_row = [surname, name, second_name, phone_num]
         connection = sql.connect('data_base')
         cursor = connection.cursor()
@@ -88,6 +108,7 @@ class MainWindow(QtWidgets.QMainWindow, MainWindow.Ui_MainWindow):
         cursor.close()
         connection.commit()
         connection.close()
+        self.read_db()
         self.link = 0
         self.show_items(self.link)
 
